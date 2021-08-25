c27c698f6b1c4b8aa9ad0fc4408d2e871854ce7f
Rocco <jack_6667@yahoo.com.tw>
2021-08-25 21:19:29 +0800

    修改為v4版本

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,22 @@
-/*
-example:
-google sheet url:https://docs.google.com/spreadsheets/d/gbnm1212fdsf5454g5fds4g5fd4sngf/edit#gid=0
-google_file_feed_id = 'gbnm1212fdsf5454g5fds4g5fd4sngf'
-*/
+
 const
-  google_file_feed_id = '1cY0YYL67Us5SotGM3L9gIxWkGdRkDc5SuthMNaNGQjg',
-  url = `https://docs.google.com/spreadsheets/d/e/2PACX-1vQw7DcgEATWH8kWIrDwv6FiiUyRV0fR6CakWSxKuHIWbF0Q1OKiFmcAqA9ne82DQ2RDUuSYoY00SXXi/pubhtml?gid=366642098&single=true`,
+  worksheet_id = '15PPK8YU4TF1R9DmZ-86Rqzpv38CWzsgykdNXBF3FEiU',
+  tab_name = '8888',
+  key = 'AIzaSyBNhC6OI2QeHm2r3vdvlhiWx0ltzhuWKBk',
+  url = 'https://sheets.googleapis.com/v4/spreadsheets/'+worksheet_id+'/values/'+tab_name+'?alt=json&key='+key,
   duration = 3500; // 拉霸效果執行多久
 var cheer_data, btn = $('.btn-start');
 fetch(url)
   .then(res => res.json())
   .then(data => {
-    var all_data = data.feed.entry;
-    cheer_data = all_data.map(d => `${d.gsx$姓名.$t} - ${d.gsx$電話.$t}`).
+    var header = data.values[0];
+    var all_data = data.values.slice(1).map(function(d){
+      tmp = {}
+      header.forEach(function(h,i){ tmp[h] = d[i] });
+      return tmp
+    })
+
+    cheer_data = all_data.map(d => `${d.姓名} - ${d.電話}`).
       filter(onlyUnique).map(d => `${d.split(" - ")[0]} - ${mask(d.split(" - ")[1])}`);
     $('body').append(`<style>${spin_style(cheer_data.length)}</style>`);
     var randChk = Array.from(Array(cheer_data.length).keys());
@@ -22,6 +26,7 @@ fetch(url)
       // 禁止按鈕再被點擊
       $(this).addClass('not-allow');
       const chooseOne = toggle => {
+        console.log("Sample count:" + randChk.length)
         // 清空、插入選項
         let input = document.querySelector('.wrap');
         input.innerHTML = '';
